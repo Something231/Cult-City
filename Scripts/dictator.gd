e69cf836d5ba210e226d23dc5fc985bf1622a158
Something231 <119870385+Something231@users.noreply.github.com>
2024-08-05 20:49:38 +0800

    implementing multiselection, bugged code, cant run

--- Scripts/dictator.gd
+++ Scripts/dictator.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var building = false
+var building: bool = false
 var buildmaterial = "_"
+var buildstate: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +16,7 @@ func _process(delta):
 func build_state(button):
 	building = not building
 	buildmaterial = button
+var last_selected = Vector2i(0, 0)
+var selection_state: int = 0
+
+
